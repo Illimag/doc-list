092c9851afdb3dacee62508fc9c7bfc3df5e39a0
jaemnkm <jaemnkm@gmail.com>
2019-01-08 18:00:38 -0800

    convert to recursion

--- 17576_names_of_god/name_of_god_recursion/AAA_ZZZ_time_calculation.c
+++ 17576_names_of_god/name_of_god_recursion/AAA_ZZZ_time_calculation.c
@@ -1,16 +1,99 @@
 #include <stdio.h>
- 
-int main() {
-    char a,b,c;
-    char name;
-    for(a='A';a<='Z';++a){
-            for(b='A';b<='Z';++b){
-                for(c='A';c<='Z';++c){
-                    name = a + b + c;
-                    printf("%c\t",name);
-                }
+char first_letter,second_letter,third_letter;
+
+/*
+void rec_c(int c,int b,int a) { // k-loop
+    if (!(c<='Z')) return;
+    printf("%c%c%c\n",a,b,c);
+    rec_c(c+1,a,b); // recurse
+}
+
+void rec_b(int b,int a, int c) { // j-loop
+    if (!(b<='Z')) return;
+    rec_c(0,a,b); // inner loop
+    rec_b(b+1,a,c); // recurse
+}
+
+void rec_a(int a,int b,int c) { // i-loop
+    if (!(a<='Z')) return;
+    rec_b(0,a,b); // inner loop
+    rec_a(a+1,b,c); // recurse
+}
+
+int main(){
+    rec_a(a,b,c);
+}
+ */
+
+
+
+    for (first_letter = start_letter; first_letter <= end_letter; first_letter++) {
+        for (second_letter = start_letter; second_letter <= end_letter; second_letter++) {
+            for (third_letter = start_letter; third_letter <= end_letter; third_letter++) {
+
             }
         }
+    }
+
+
+
+
+
+void rec_c(char a,char b,char c,char end_letter) { // k-loop
+    if (!(c <= end_letter)) return;
+
+    rec_c(a,b,c+1,end_letter); // recurse
 }
+
+void rec_b(char a,char b,char end_letter, char end_letter) { // j-loop
+    if (!(b <= end_letter)) return;
+    rec_c(a,b,start_letter,end_letter); // inner loop
+    rec_b(a,b+1,end_letter); // recurse
+}
+
+void rec_a(char a, char end_letter) { // i-loop
+    if (!(a <= end_letter)) return;
+    rec_b(a,start_letter,end_letter); // inner loop
+    rec_a(a+1,end_letter); // recurse
+}
+...
+rec_a(start_letter,end_letter);
+
+
+
+
+for (i = 0; i < q; i++) {
+        for (j = 0; j < p; j++) {
+            for (k = 0; k < r; k++) {
+
+            }
+        }
+    }
+
+
+
+void rec_k(int k,int r,int i,int j) { // k-loop
+    if (!(k<r)) return;
+
+    rec_k(k+1,r,i,j); // recurse
+}
+
+void rec_j(int j,int p,int i,int r) { // j-loop
+    if (!(j<p)) return;
+    rec_k(0,r,i,j); // inner loop
+    rec_j(j+1,p,i,r); // recurse
+}
+
+void rec_i(int i,int q,int p,int r) { // i-loop
+    if (!(i<q)) return;
+    rec_j(0,p,i,r); // inner loop
+    rec_i(i+1,q,p,r); // recurse
+}
+...
+rec_i(0,q,p,r);
+
+
+
+
 
 
--- 17576_names_of_god/name_of_god_recursion/name_of_god.c
+++ 17576_names_of_god/name_of_god_recursion/name_of_god.c
@@ -2,8 +2,9 @@
 int NameOfGod(int number);
 // char a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
 // char array[26]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
-// char name = "finished";
+// char name;
 int array_test[26]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+int test;
 
 int main(){
     NameOfGod(9);
@@ -13,7 +14,7 @@ int NameOfGod(int number){
     if(number==0){
         return(1);
     }else{
-        printf("%d",array_test[number-1]);
-        NameOfGod(--number);
+            printf("%d",array_test[number-1]);
+            NameOfGod(--number);
     }
 }
