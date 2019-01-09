bf0097107a268a3731c549ac85c26d3a4cebc042
jaemnkm <jaemnkm@gmail.com>
2019-01-08 17:04:13 -0800

    NameOfGod recursive call

--- 17576_names_of_god/name_of_god_recursion/name_of_god.c
+++ 17576_names_of_god/name_of_god_recursion/name_of_god.c
@@ -3,7 +3,7 @@ int NameOfGod(int number);
 // char a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
 // char array[26]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
 // char name = "finished";
-int array_test[26]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
+int array_test[26]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
 
 int main(){
     NameOfGod(9);
@@ -11,12 +11,9 @@ int main(){
 
 int NameOfGod(int number){
     if(number==0){
-        return(0);
+        return(1);
     }else{
-        for(number=number;number>0;){
-            printf("%d",array_test[number-1]);
-            NameOfGod(--number);
-        }
-        printf(" \n");
+        printf("%d",array_test[number-1]);
+        NameOfGod(--number);
     }
 }
