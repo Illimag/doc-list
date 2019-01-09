0fd6c5f1b0c49209c68d0ee72b7f25a3b444bc95
jaemnkm <jaemnkm@gmail.com>
2019-01-08 16:23:22 -0800

    recursion function

--- 17576_names_of_god/name_of_god_recursion/name_of_god.c
+++ 17576_names_of_god/name_of_god_recursion/name_of_god.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-
-char a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
-char array[26]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
-char name;
-
 int NameOfGod(int number);
+// char a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
+// char array[26]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+// char name = "finished";
 
 int main(){
     NameOfGod(9);
 }
 
 int NameOfGod(int number){
-    iteration();
-}
-
-int iteration(){
-    for(number=number;number>0;number--){
+    for(number=number;number>=0;--number){
         if(number>0){
-            for(array[number]='A';array[number]<='Z';++array[number]){
-                name = array[number] + name;
-                NameOfGod(number--);
-            }
+
+            // for(array[number]='A';array[number]<='Z';++array[number]){
+
+            // name = array[number] + name;
+            printf("%d\n",number);
+            //}
+
         }else{
-            printf("%c\n",name);
+            //printf("%c\n",name);
+            printf("finshed");
+            break;
         }
     }
 }
